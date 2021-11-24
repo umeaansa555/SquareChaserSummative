a5d9cc0a875fb80d7cd83f42ea30a2a948fae6bd
P4 <umeaansa555@ed.amdsb.ca>
2021-11-24 14:08:43 -0500

    goal working

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,14 +29,18 @@ namespace SquareChaser
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.P1Score = new System.Windows.Forms.Label();
             this.P2Score = new System.Windows.Forms.Label();
+            this.engine = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
             // 
             // P1Score
             // 
             this.P1Score.AutoSize = true;
+            this.P1Score.BackColor = System.Drawing.Color.Transparent;
             this.P1Score.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.P1Score.ForeColor = System.Drawing.Color.WhiteSmoke;
             this.P1Score.Location = new System.Drawing.Point(82, 9);
             this.P1Score.Name = "P1Score";
             this.P1Score.Size = new System.Drawing.Size(94, 25);
@@ -46,20 +50,30 @@ namespace SquareChaser
             // P2Score
             // 
             this.P2Score.AutoSize = true;
+            this.P2Score.BackColor = System.Drawing.Color.Transparent;
             this.P2Score.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.P2Score.ForeColor = System.Drawing.Color.WhiteSmoke;
             this.P2Score.Location = new System.Drawing.Point(182, 9);
             this.P2Score.Name = "P2Score";
             this.P2Score.Size = new System.Drawing.Size(94, 25);
             this.P2Score.TabIndex = 1;
             this.P2Score.Text = "P2Score";
             // 
+            // engine
+            // 
+            this.engine.Enabled = true;
+            this.engine.Interval = 20;
+            this.engine.Tick += new System.EventHandler(this.engine_Tick);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DimGray;
             this.ClientSize = new System.Drawing.Size(384, 361);
             this.Controls.Add(this.P2Score);
             this.Controls.Add(this.P1Score);
+            this.DoubleBuffered = true;
             this.Name = "Form1";
             this.Text = "Form1";
             this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
@@ -74,6 +88,7 @@ namespace SquareChaser
 
         private System.Windows.Forms.Label P1Score;
         private System.Windows.Forms.Label P2Score;
+        private System.Windows.Forms.Timer engine;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -8,16 +8,23 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+// Square Chaser: 2 player game
+// A. Ansari midterm summative Nov 2021
+
+
 namespace SquareChaser
 {
     public partial class Form1 : Form
     {
 
-        Rectangle player1 = new Rectangle();
-        Rectangle player2 = new Rectangle();
-        Rectangle speedBoost = new Rectangle();
-        Rectangle goal = new Rectangle();
+        Rectangle player1 = new Rectangle(0, 0, 20, 20);
+        Rectangle player2 = new Rectangle(30, 0, 20, 20);
+        Rectangle speedBoost = new Rectangle(0, 17, 10, 10);
+        Rectangle goal = new Rectangle(0, 49, 10, 10);
 
+        Random positionGen = new Random();
+        
+        
         int player1Score = 0;
         int player2Score = 0;
 
@@ -40,6 +47,11 @@ namespace SquareChaser
         public Form1()
         {
             InitializeComponent();
+            goal.X = positionGen.Next(0, 381);
+            goal.Y = positionGen.Next(0, 381);
+
+            speedBoost.X = positionGen.Next(0, 381);
+            speedBoost.Y = positionGen.Next(0, 381);
         }
 
         private void Form1_KeyDown(object sender, KeyEventArgs e)
@@ -114,6 +126,66 @@ namespace SquareChaser
             e.Graphics.FillRectangle(whiteBrush, goal);
             e.Graphics.FillRectangle(yellowBrush, speedBoost);
 
+        }
+
+        private void engine_Tick(object sender, EventArgs e)
+        {
+            //move player 1 
+            if (wDown == true && player1.Y > 0)
+            {
+                player1.Y -= playerSpeed;
+            }
+
+            if (sDown == true && player1.Y < this.Height - player1.Height)
+            {
+                player1.Y += playerSpeed;
+            }
+
+
+            ////in progress. right works, left has problems
+            if (aDown == true && player1.X > 0)
+            {
+                player1.X -= playerSpeed;
+            }
+
+            if (dDown == true && player1.X < this.Width - player1.Width)
+            {
+                player1.X += playerSpeed;
+            }
+
+
+            //move player 2 
+            if (upArrowDown == true && player2.Y > 0)
+            {
+                player2.Y -= playerSpeed;
+            }
+
+            if (downArrowDown == true && player2.Y < this.Height - player2.Height)
+            {
+                player2.Y += playerSpeed;
+            }
+
+
+            ////in progress, movement works but its buggy
+            if (leftArrowDown == true && player2.X > 0)
+            {
+                player2.X -= playerSpeed;
+            }
+            if (rightArrowDown == true && player2.X < this.Width - player2.Width)
+            {
+                player2.X += playerSpeed;
+            }
+
+
+            ////update position of objects
+            if(player1.IntersectsWith(goal) || player2.IntersectsWith(goal))
+            {
+                goal.X = positionGen.Next(0, 381);
+                goal.Y = positionGen.Next(0, 381);
+            }
+            
+
+            Refresh();
         }
     }
 }
