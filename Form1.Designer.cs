4977a69dba22a62b8a7dbf392ccaced4f3c604c1
P4 <umeaansa555@ed.amdsb.ca>
2021-11-25 13:28:10 -0500

    design window

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -84,7 +84,7 @@ namespace SquareChaser
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.PaleGoldenrod;
-            this.ClientSize = new System.Drawing.Size(384, 361);
+            this.ClientSize = new System.Drawing.Size(400, 400);
             this.Controls.Add(this.winLabel);
             this.Controls.Add(this.P2Score);
             this.Controls.Add(this.P1Score);
